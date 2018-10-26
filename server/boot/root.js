f5eba844fda8b78a3651049f55fe88d3cf1ff260
fluper36 <shadabsaifi03@gmail.com>
2018-10-26 12:51:58 +0530

    User Validations

--- server/boot/root.js
+++ server/boot/root.js
@@ -4,8 +4,5 @@ module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
-  router.get('/api/users',  (req, res)=>{
-    res.send("hello")
-  })
   server.use(router);
 };
