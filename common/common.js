3e8198debba6d22baa918eb821d14e8fba901e7b
shekhar tyagi <shekhartyagi26@gmail.com>
2018-11-06 12:55:22 +0530

    modification in crud operations

--- common/common.js
+++ common/common.js
@@ -1,68 +1,70 @@
 const bcrypt = require('bcrypt'),
-      constant = require('./constant'),
-      common = require('./common')
+  constant = require('./constant'),
+  common = require('./common')
+
 let route = {
-    http:{  },
-    accepts:{  },
-    returns:{ arg:"data", type:"object" }
-}, err, reqParameter, body = { };
+    http: {},
+    accepts: {},
+    returns: { arg: "data", type: "object" }
+  },
+  err, reqParameter, body = {};
 
-const createHash = (password, cb)=>{
-    bcrypt.genSalt(constant.SALT_ROUNDS, (err, salt)=>{
-        bcrypt.hash(password, salt, (err, hash)=>{
-            if(err)
-                cb(err);
-            else
-                cb(null, hash);
-        });
+const createHash = (password, cb) => {
+  bcrypt.genSalt(constant.SALT_ROUNDS, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+      if (err)
+        cb(err);
+      else
+        cb(null, hash);
     });
+  });
 };
 
-const hashCompare = (oldPassword, dbPassword, cb)=>{
-    bcrypt.compare(oldPassword, dbPassword, (err, match)=>{
-        if(err)
-            cb(err);
-        else
-            cb(null, match);
-    });
-};
-
-const validation = (object)=>{
-    for(var key in object)
-        if(!object[key])
-            return `${key} can't be blank.`;
-};
-
-const router = (path, verb, arg)=>{
-    route.http = { path:path, verb:verb };
-    if(verb == 'get')
-        route['accepts'] = { arg:arg, type:'string', http:{ source:'query' } };
+const hashCompare = (oldPassword, dbPassword, cb) => {
+  bcrypt.compare(oldPassword, dbPassword, (err, match) => {
+    if (err)
+      cb(err);
     else
-        route['accepts'] = { arg:'data', type:'object', http:{ source:'body' } };
-    return route;    
+      cb(null, match);
+  });
 };
 
-const beforeRemote = (context, parameter, next)=>{
+const validation = (object) => {
+  for (var key in object)
+    if (!object[key])
+      return `${key} can't be blank.`;
+};
 
-    if(context.method.http['verb'] == 'post')
-        reqParameter = context.req.body;
-    else
-        reqParameter = context.req.query;
-    parameter.map((x)=>{
-        body[x] = reqParameter[x];   
-    });
-    err = validation(body)
-    if(err)
-        next(err);
-    else
-        next();
+const router = (path, verb, arg) => {
+  route.http = { path: path, verb: verb };
+  if (verb == 'get')
+    route['accepts'] = { arg: arg, type: 'string', http: { source: 'query' } };
+  else
+    route['accepts'] = { arg: 'data', type: 'object', http: { source: 'body' } };
+  return route;
+};
+
+const beforeRemote = (context, parameter, next) => {
+
+  if (context.method.http['verb'] == 'post')
+    reqParameter = context.req.body;
+  else
+    reqParameter = context.req.query;
+  parameter.map((x) => {
+    body[x] = reqParameter[x];
+  });
+  err = validation(body)
+  if (err)
+    next(err);
+  else
+    next();
 };
 
 module.exports = {
 
-    createHash,
-    hashCompare,
-    validation,
-    router,
-    beforeRemote
+  createHash,
+  hashCompare,
+  validation,
+  router,
+  beforeRemote
 };
--- common/models/user.js
+++ common/models/user.js
@@ -4,9 +4,9 @@ const userApi = require('./userAPI');
 module.exports = (User) => {
 
   // 1st API for Signup
-  User.signup = (userData, cb)=>{
-    userApi.signup(User, userData, (err, user)=>{
-      if(err)
+  User.signup = (userData, cb) => {
+    userApi.signup(User, userData, (err, user) => {
+      if (err)
         cb(err);
       else
         cb(null, user);
@@ -14,53 +14,53 @@ module.exports = (User) => {
   };
 
   User.remoteMethod('signup', common.router('/signup', 'post', 'data'));
-  User.beforeRemote('signup',(context, user, next)=>common.beforeRemote(context, ['name','email','password'], next));
+  User.beforeRemote('signup', (context, user, next) => common.beforeRemote(context, ['name', 'email', 'password'], next));
 
   // 2rd API for send OTP
   User.sendOTP = (email, cb) => {
-    userApi.sendOTP(User, email, (err, user)=>{
-      if(err)
+    userApi.sendOTP(User, email, (err, user) => {
+      if (err)
         cb(err);
       else
         cb(null, user);
-    }); 
+    });
   };
- 
+
   User.remoteMethod('sendOTP', common.router('/sendOTP', 'get', 'email'));
-  User.beforeRemote('sendOTP',(context, user, next)=>common.beforeRemote(context, ['email'], next));
+  User.beforeRemote('sendOTP', (context, user, next) => common.beforeRemote(context, ['email'], next));
 
   // 3th API for Verify OTP
   User.verifyOTP = (userData, cb) => {
-    userApi.verifyOTP(User, userData, (err, user)=>{
-      if(err)
+    userApi.verifyOTP(User, userData, (err, user) => {
+      if (err)
         cb(err);
       else
         cb(null, user);
     });
   };
-  
-  User.remoteMethod('verifyOTP',  common.router('/verifyOTP', 'post', 'data'))
-  User.beforeRemote('verifyOTP',(context, user, next)=>common.beforeRemote(context, ['email', 'OTP'], next));
+
+  User.remoteMethod('verifyOTP', common.router('/verifyOTP', 'post', 'data'))
+  User.beforeRemote('verifyOTP', (context, user, next) => common.beforeRemote(context, ['email', 'OTP'], next));
 
 
   // 4th API for User Detail
-  User.getUser = function(id, cb){
-    userApi.getUser(User, id, (err, user)=>{
-      if(err)
+  User.getUser = function(id, cb) {
+    userApi.getUser(User, id, (err, user) => {
+      if (err)
         cb(err);
       else
         cb(null, user);
     });
   };
 
-  User.remoteMethod('getUser',  common.router('/getUser', 'get', 'id'))
-  User.beforeRemote('getUser', (context, user, next)=>common.beforeRemote(context, ['id'], next));
+  User.remoteMethod('getUser', common.router('/getUser', 'get', 'id'))
+  User.beforeRemote('getUser', (context, user, next) => common.beforeRemote(context, ['id'], next));
 
 
   // 5th API for get ALl User list
-  User.getAllUser = function(id, cb){
-    userApi.getAllUser(User, (err, user)=>{
-      if(err)
+  User.getAllUser = function(id, cb) {
+    userApi.getAllUser(User, (err, user) => {
+      if (err)
         cb(err);
       else
         cb(null, user);
@@ -71,36 +71,36 @@ module.exports = (User) => {
 
   //6th API for Detete User
   User.deleteUser = (id, cb) => {
-    userApi.deleteUser(User, id, (err, user)=>{
-      if(err)
+    userApi.deleteUser(User, id, (err, user) => {
+      if (err)
         cb(err);
       else
         cb(null, user);
     });
   };
-  
+
   User.remoteMethod('deleteUser', common.router('/deleteUser', 'get', 'id'))
-  User.beforeRemote('deleteUser',(context, user, next)=>common.beforeRemote(context, ['id'], next));
+  User.beforeRemote('deleteUser', (context, user, next) => common.beforeRemote(context, ['id'], next));
 
 
   // 7th API for edit User Profile
   User.editUser = (userData, cb) => {
-    userApi.editUser(User, userData, (err, user)=>{
-      if(err)
+    userApi.editUser(User, userData, (err, user) => {
+      if (err)
         cb(err);
       else
         cb(null, user);
     });
   };
-  
+
   User.remoteMethod('editUser', common.router('/editUser', 'post', 'data'))
-  User.beforeRemote('editUser',(context, user, next)=>common.beforeRemote(context, ['id','name'], next));
+  User.beforeRemote('editUser', (context, user, next) => common.beforeRemote(context, ['id', 'name'], next));
 
 
   // 8th API for resetPassword Password
   User.resetPassword = (userData, cb) => {
-    userApi.resetPassword(User, userData, (err, user)=>{
-      if(err)
+    userApi.resetPassword(User, userData, (err, user) => {
+      if (err)
         cb(err);
       else
         cb(null, user);
@@ -108,13 +108,13 @@ module.exports = (User) => {
   };
 
   User.remoteMethod('resetPassword', common.router('/resetPassword', 'post', 'data'))
-  User.beforeRemote('resetPassword',(context, user, next)=>common.beforeRemote(context, ['email','password','newPassword'], next));
+  User.beforeRemote('resetPassword', (context, user, next) => common.beforeRemote(context, ['email', 'password', 'newPassword'], next));
 
 
   // 9th API for Login
-  User.login = (userData, cb)=>{ 
-    userApi.login(User, userData, (err, user)=>{
-      if(err)
+  User.login = (userData, cb) => {
+    userApi.login(User, userData, (err, user) => {
+      if (err)
         cb(err);
       else
         cb(null, user);
@@ -122,6 +122,6 @@ module.exports = (User) => {
   };
 
   User.remoteMethod('login', common.router('/login', 'post', 'data'));
-  User.beforeRemote('login',(context, user, next)=>common.beforeRemote(context, ['email','password'], next));
+  User.beforeRemote('login', (context, user, next) => common.beforeRemote(context, ['email', 'password'], next));
 
 }
